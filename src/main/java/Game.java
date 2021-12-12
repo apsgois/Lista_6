8274064ff0cd803d56ee58d7299fb1f09c246480
apsgois <apsgois@outlook.com>
2021-12-12 19:45:48 -0300

    first commit

---
+++ src/main/java/Game.java
@@ -0,0 +1,10 @@
+
+public class Game {
+    private int rank;
+    private String name;
+    private int year;
+    private String platform;
+    private String global_sales;
+    private String publisher;
+
+}
---
+++ src/main/java/Platform.java
@@ -0,0 +1,3 @@
+public enum Platform {
+    PS4,PC,XB,Wii,GB,GBA,X360,PS3,PS2;
+}
